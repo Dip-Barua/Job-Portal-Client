0d2cae62a115b88cf7ecbb8156dc31866c9e1725
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-12-12 12:24:03 +0600

    Added : Custom animation setiup for login page (React lottie)

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,21 +1,32 @@
 import Lottie from "lottie-react";
-import registerLottieData from '../../assets/lottie/register.json'
+import loginLottieData from '../../assets/lottie/login.json'
 const SignIn = () => {
 
+const handleSignIn = e => {
+    e.preventDefault();
+  const form = e.target;
+  const email = form.email.value;
+  const password = form.password.value;
+  // validate email and password here...
+  console.log(email, password);
+  // send request to server to authenticate user...
 
 
-    
+
+}
+
+
     return (
         <div>
         <div className="hero bg-base-200 min-h-screen">
 <div className="hero-content flex-col lg:flex-row-reverse">
 <div className="text-center lg:text-left w-3/6">
-  <Lottie animationData={registerLottieData}></Lottie>
+  <Lottie animationData={loginLottieData}></Lottie>
 </div>
 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
 <h1 className="text-5xl font-bold ml-8 mt-4">Login now!</h1>
 
-  <form  className="card-body">
+  <form onSubmit={handleSignIn} className="card-body">
     
     <div className="form-control">
         
@@ -34,7 +45,7 @@ const SignIn = () => {
       </label>
     </div>
     <div className="form-control mt-6">
-      <button  className="btn btn-primary">Register</button>
+      <button  className="btn btn-primary">Login</button>
     </div>
   </form>
 </div>
