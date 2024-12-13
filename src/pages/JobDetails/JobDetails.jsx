c4ccb9aebbf5e9a7dc7547c4b398c140bd25b4bd
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-12-13 22:25:47 +0600

    Added : job apply component created with server connectivity

--- src/pages/JobDetails/JobDetails.jsx
+++ src/pages/JobDetails/JobDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const JobDetails = () => {
 
@@ -8,6 +8,8 @@ const JobDetails = () => {
         <div className="w-6/12 mx-auto my-5 text-center">
             <h2 className="text-3xl">job details for : {job.title}</h2>
             <p className="text-xl">Apply for : {job.company}</p>
+            <Link to={`/jobApply/${job._id}`}>            <button className="btn btn-primary">Apply Now</button>
+</Link>
         </div>
     );
 };
